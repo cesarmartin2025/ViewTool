ebeeb44a0997ddd06351e2b01cb6d3b7c3fff5e8
cesar <cesarmartin@paucasesnovescifp.cat>
2025-10-15 10:42:20 +0200

    Initial commit - Project structure created

---
+++ src/main/java/martin/viewtool/ViewTool.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package martin.viewtool;
+
+/**
+ *
+ * @author cesar
+ */
+public class ViewTool {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
